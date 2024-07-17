fbd84eca18113583f330896dfce2f14037ce0729
Habsoum64 <Habsoum64@users.noreply.github.com>
2024-07-17 15:09:24 +0000

    updated admin js scripting

--- admin_dashboard/admin_script.js
+++ admin_dashboard/admin_script.js
@@ -32,35 +32,72 @@ function fetchUsers() {
         url: 'admin_actions.php',
         method: 'POST',
         data: { action: 'fetch_users' },
-        success: function() {
-            console.log('Users fetched successfuly');
+        success: function (response) {
+            const users = JSON.parse(response);
+            const usersTable = document.getElementById('users');
+            usersTable.innerHTML = '';
+            users.forEach(user => {
+                usersTable.innerHTML += `
+                    <tr>
+                        <td>${user.id}</td>
+                        <td>${user.name}</td>
+                        <td>${user.email}</td>
+                        <td>${user.user_role}</td>
+                        <td><button type="button" class="btn btn-outline-primary m-2" onClick=deleteUser(${user.id})>Delete</button></td>
+                    </tr>
+                `;
+            });
         },
-        error: function(xhr, status, error) {
+        error: function (xhr, status, error) {
             console.error('Error fetching users:', error);
             alert('Failed to fetch users.');
         }
     });
 }
 
-function deleteUser(userId) {
+function deleteUser(user_id) {
     $.ajax({
-        url: 'admin_actions',
+        url: 'admin_actions.php',
         method: 'POST',
-        data: { 
-            action: deleteUser,
-            userId: userId 
+        data: { action: 'delete_user', 'user_id': user_id },
+        success: function (response) {
+            console.log("User deleted successfully.");
+            alert('User deleted successfully.');
         },
-        success: function(data) {
-            if (data.success) {
-                fetchUsers();
-                showNotification('User deleted successfully!');
-            } else {
-                showNotification('Failed to delete User.');
-            }
+        error: function (xhr, status, error) {
+            console.error('Error fetching users:', error);
+            alert('Failed to fetch users.');
+        }
+    });
+}
+
+function fetchProducts() {
+    $.ajax({
+        url: 'admin_actions.php',
+        method: 'POST',
+        data: { action: 'fetch_products' },
+        success: function (response) {
+            const products = JSON.parse(response);
+            const productsTable = document.getElementById('products');
+            productsTable.innerHTML = '';
+            products.forEach(product => {
+                productsTable.innerHTML += `
+                    <tr>
+                        <td>${product.product_id}</td>
+                        <td>${product.product_cat}</td>
+                        <td>${product.product_brand}</td>
+                        <td>${product.product_title}</td>
+                        <td>${product.product_price}</td>
+                        <td>${product.product_desc}</td>
+                        <td>${product.product_keywords}</td>
+                        <td><a class='btn btn-sm btn-primary' href='#'>Delete</a></td>
+                    </tr>
+                `;
+            });
         },
-        error: function(xhr, status, error) {
-            console.error('Error deleting user:', error);
-            alert('Failed to delete user.');
+        error: function (xhr, status, error) {
+            console.error('Error fetching products:', error);
+            alert('Failed to fetch products.');
         }
     });
 }
@@ -70,35 +107,33 @@ function fetchOrders() {
         url: 'admin_actions.php',
         method: 'POST',
         data: { action: 'fetch_orders' },
-        success: function(response) {
-            console.log('Fetched data:', response);
-            const orderList = $('#orders');
-            orderList.empty();
-            response.forEach(order => {
-                const orderItem = $('<tr></tr>');
-                order.forEach(info => {
-                    const record = $('<td></td>').html(info);
-                    orderItem.append(record);
-                })
-                const entry = $('<td></td>');
-                const confirmButton = $('<button>Confirm</button>').data('class', "btn btn-sm btn-primary").data('id', order.id);
-                confirmButton.click(function() {
-                    const orderId = $(this).data('id');
-                    updateOrderStatus(orderId, "Confirmed");
-                });
-                const cancelButton = $('<button>Cancel</button>').data('class', "btn btn-sm btn-primary").data('id', order.id);
-                cancelButton.click(function() {
-                    const orderId = $(this).data('id');
-                    updateOrderStatus(orderId, "Canceled");
-                });
-                entry.append(confirmButton);
-                entry.append(deleteButton);
-                userItem.append(entry);
-                userList.append(userItem);
+        success: function (response) {
+            const orders = JSON.parse(response);
+            const ordersTable = document.getElementById('orders');
+            ordersTable.innerHTML = '';
+            orders.forEach(order => {
+                ordersTable.innerHTML += `
+                    <tr>
+                        <td>${order.order_id}</td>
+                        <td>${order.customer_id}</td>
+                        <td>${order.invoice_no}</td>
+                        <td>${order.order_date}</td>
+                        <td>${order.order_status}</td>
+                        <td>
+                            <a class='btn btn-sm btn-primary' href='#'>Confirm</a>
+                            <a class='btn btn-sm btn-primary' href='#'>Cancel</a>
+                        </td>
+                    </tr>
+                `;
             });
         },
+        error: function (xhr, status, error) {
+            console.error('Error fetching orders:', error);
+            alert('Failed to fetch orders.');
+        }
     });
 }
+
 
 function updateOrderStatus(orderId, status) {
     $.ajax({
