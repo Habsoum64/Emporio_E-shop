9802f986ae1f5afcb3105e9623f31b7727264f28
Habsoum64 <Habsoum64@users.noreply.github.com>
2024-07-23 06:37:09 +0000

    update user_nav

--- js/general_script.js
+++ js/general_script.js
@@ -19,11 +19,22 @@ function userNav() {
                     <i class="fas fa-user fa-2x"></i>
                 </a>
                 <div class="dropdown-menu m-0 bg-secondary rounded-0">
+        `;
+        if (user_role == "customer") {
+            userNav.innerHTML += `
                     <a href="../user_dashboard/index.html" class="dropdown-item">Dashboard</a>
+                `
+        } else if (user_role == "admin") {
+            userNav.innerHTML += `
+                    <a href="../admin_dashboard/home.html" class="dropdown-item">Dashboard</a>
+                `
+        }
+
+        user_nav.innerHTML += `
                     <a href="../actions/logout.php" class="dropdown-item">Logout</a>
                 </div>
             </div>
-        `;
+        `
     }
 }
 
